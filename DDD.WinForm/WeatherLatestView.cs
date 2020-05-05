553e4ee0f53326fb344494e2df5b2fc25e7ae59f
oz-wizard <35863549+oz-wizard@users.noreply.github.com>
2020-05-05 13:59:51 +0900

    関数・定数の共通化

---
+++ DDD.WinForm/WeatherLatestView.cs
@@ -0,0 +1,35 @@
+﻿using DDD.WinForm.Common;
+using DDD.WinForm.Common.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DDD.WinForm
+{
+    public partial class WeatherLatestView : Form
+    {
+        public WeatherLatestView()
+        {
+            InitializeComponent();
+        }
+
+        private void LatestButton_Click(object sender, EventArgs e)
+        {
+            var dt = WeatherSQLite.GetLatest(Convert.ToInt32(AreaIdTextBox.Text));
+            if(dt.Rows.Count > 0)
+            {
+                DataDateLabel.Text    = dt.Rows[0]["DataDate"].ToString();
+                ConditionLabel.Text   = dt.Rows[0]["Condition"].ToString();
+                TemperatureLabel.Text =
+                    CommonFunc.RoundString(Convert.ToSingle(dt.Rows[0]["Temperature"]), CommonConst.TemperatureDecimalPoint) + CommonConst.TemperatureUnitName;
+            }
+        }
+    }
+}
